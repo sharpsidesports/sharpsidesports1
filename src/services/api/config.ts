5d63030d74e02e495b988d302035dab6f4aedfcd
kielerrr <ryan@kielventures.com>
2025-01-21 12:47:30 -0500

    fixed vite config

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
-  BASE_URL: '/api',
+  // BASE_URL: '/api',
+  BASE_URL: import.meta.env.VITE_DG_API_URL || 'https://feeds.datagolf.com',
   API_KEY: 'cf5b806066038ad69a752686db8f',
 } as const;
 
