ce58dff0ecfd9c37cb983d67fcf15c070bd8f3da
sharpsidesports <sharpsideconsulting@gmail.com>
2025-06-17 21:38:32 -0400

    Remove 'What Our Pros Are Using This Week' section from betting model page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -64,7 +64,6 @@ export default function Dashboard() {
         <PerformanceTable />
       </div>
 
-      <CourseInsights />
       <GolferList />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
